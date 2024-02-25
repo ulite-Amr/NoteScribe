87f040fc127b468264d34188326f5cf1e7fae017
ulite-Amr <aamar302@yahoo.com>
2024-02-25 14:54:43 +0200

    update the libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,14 +26,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
     implementation("com.google.android.material:material:1.12.0-alpha03")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.0-alpha03")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0-alpha03")
 }
