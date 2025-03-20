a6dcb72e59ef543e72f93abef0a8ce73c4bb99a8
ulite-Amr <aamar302@yahoo.com>
2025-03-20 04:21:13 +0200

    Feat: Implement basic layout for HomeScreen and update MainActivity
    - Add a `Column` composable to `HomeScreen` to structure the layout.
    - Add `fillMaxSize` modifier to `HomeScreen`'s `Column`.
    - Add `enableEdgeToEdge` to `MainActivity`.
    - Added content to `HomeScreen` to be rendered in the `Column`.

--- app/src/main/java/com/ulite/notescribe/HomeScreen.kt
+++ app/src/main/java/com/ulite/notescribe/HomeScreen.kt
@@ -1,11 +1,17 @@
 package com.ulite.notescribe
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
-    Text(text = "Home Screen")
+    Column(modifier = modifier.fillMaxSize()) {
+        // content
+        Text(text = "Home Screen")
+
+    }
 
 }
--- app/src/main/java/com/ulite/notescribe/MainActivity.kt
+++ app/src/main/java/com/ulite/notescribe/MainActivity.kt
@@ -16,7 +16,7 @@ import com.ulite.notescribe.ui.theme.NoteScribeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             NoteScribeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
