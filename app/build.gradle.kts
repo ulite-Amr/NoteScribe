ed8a8abc3b765ef0d63fb710b9a63f6a5053bb49
ulite-Amr <aamar302@yahoo.com>
2025-03-20 04:43:48 +0200

    Dependencies: Update core-ktx, lifecycle-runtime-ktx, activity-compose, compose-bom, junit, espresso-core dependencies
    - Updated `androidx.core:core-ktx` to version 1.15.0.
    - Updated `androidx.lifecycle:lifecycle-runtime-ktx` to version 2.8.7.
    - Updated `androidx.activity:activity-compose` to version 1.10.1.
    - Updated `androidx.compose:compose-bom` to version 2025.03.00.
    - Updated `androidx.test.ext:junit` to version 1.2.1.
    - Updated `androidx.test.espresso:espresso-core` to version 3.6.1.
    - Removed unused Greeting and GreetingPreview composables.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,14 +46,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.15.0")
 
     // Lifecycle Runtime
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2025.03.00"))
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -61,9 +61,9 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     // Debugging
